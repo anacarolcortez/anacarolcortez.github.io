eee219d27de4f75186bea4d1b22dae4ee4d1d436
Ana Carol Cortez <carolina.cortez@aluno.ifsp.edu.br>
2021-04-06 23:05:20 -0300

    Delete hooks.js

--- hooks.js
+++
@@ -1,6 +0,0 @@
-const { openBrowser, closeBrowser } = require("taiko");
-var assert = require("assert");
-
-beforeSpec(async () => await openBrowser());
-
-afterSpec(async () => await closeBrowser());

5559043172202daa9d16f7cd96493deff52e9189
Ana Carol Cortez <carolina.cortez@aluno.ifsp.edu.br>
2021-04-06 21:56:06 -0300

    hooks

---
+++ hooks.js
@@ -0,0 +1,6 @@
+const { openBrowser, closeBrowser } = require("taiko");
+var assert = require("assert");
+
+beforeSpec(async () => await openBrowser());
+
+afterSpec(async () => await closeBrowser());
